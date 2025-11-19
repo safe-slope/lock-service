5de6439bd4c6cfc7618faf06315ffaea55712653
Andrej R <andrej.repic04@gmail.com>
2025-11-19 20:17:03 +0100

    Add LockController with a test greeting endpoint for API v1

---
+++ lock-microservice/api/src/main/java/io/github/safeslope/controller/v1/LockController.java
@@ -0,0 +1,17 @@
+package io.github.safeslope.controller.v1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class LockController {
+    private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
