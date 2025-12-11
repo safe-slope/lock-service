af1fac426d3a0e3d0fddc5d303df23ad5b6a5487
Andrej R <andrej.repic04@gmail.com>
2025-12-11 20:34:19 +0100

    Change return type of findByMacAddress to Optional<Lock>

--- lock-microservice/service/src/main/java/io/github/safeslope/lock/repository/LockRepository.java
+++ lock-microservice/service/src/main/java/io/github/safeslope/lock/repository/LockRepository.java
@@ -4,10 +4,12 @@ import io.github.safeslope.entities.Lock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /*
  JPA repo ima built-in CRUD operacije: save(), findById() ...
  */
 @Repository
 public interface LockRepository extends JpaRepository<Lock, Integer> {
-    Lock findByMacAddress(String macAddress);
+    Optional<Lock> findByMacAddress(String macAddress);
 }
