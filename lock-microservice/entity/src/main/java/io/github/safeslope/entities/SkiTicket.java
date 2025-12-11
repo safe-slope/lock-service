61e49b53526b518ab82d0bb0f5c60cc36ad80938
Andrej R <andrej.repic04@gmail.com>
2025-12-11 18:00:57 +0100

    Refactor SkiTicket entity by removing unused named queries and redundant getters/setters; enhance code clarity with Lombok annotations.

--- lock-microservice/entity/src/main/java/io/github/safeslope/entities/SkiTicket.java
+++ lock-microservice/entity/src/main/java/io/github/safeslope/entities/SkiTicket.java
@@ -3,14 +3,20 @@ package io.github.safeslope.entities;
 import java.time.LocalDateTime;
 import jakarta.persistence.*;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AccessLevel;
+import lombok.ToString;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 @Table(name = "ski_ticket")
-@NamedQueries(value = 
-    {
-        //tu vpiši uporabne querije za ski ticket
-        @NamedQuery(name = "SkiTicket.getAll", query = "SELECT st FROM SkiTicket st")
-    }
-)
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(exclude = {"skiResort"})
 public class SkiTicket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,36 +28,8 @@ public class SkiTicket {
     @Column(name = "valid_to", columnDefinition = "TIMESTAMP")
     private LocalDateTime validTo;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ski_resort_id")
+    @JsonBackReference
     private SkiResort skiResort;
-
-    //getters and setters
-    public Integer getId() {
-        return id;
-    }
-
-    public LocalDateTime getValidFrom() {
-        return validFrom;
-    }
-
-    public void setValidFrom(LocalDateTime validFrom) {
-        this.validFrom = validFrom;
-    }
-
-    public LocalDateTime getValidTo() {
-        return validTo;
-    }
-
-    public void setValidTo(LocalDateTime validTo) {
-        this.validTo = validTo;
-    }
-
-    public SkiResort getSkiResort() {
-        return skiResort;
-    }
-
-    public void setSkiResort(SkiResort skiResort) {
-        this.skiResort = skiResort;
-    }
 }
