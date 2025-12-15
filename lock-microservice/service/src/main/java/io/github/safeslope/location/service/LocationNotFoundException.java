7ad34e942875b1c17e6d819d73159c5db863f47a
nicknick75 <nika.kralj27@gmail.com>
2025-12-15 17:01:44 +0100

    Integer id v LocationNotFoundException

--- lock-microservice/service/src/main/java/io/github/safeslope/location/service/LocationNotFoundException.java
+++ lock-microservice/service/src/main/java/io/github/safeslope/location/service/LocationNotFoundException.java
@@ -1,7 +1,7 @@
 package io.github.safeslope.location.service;
 
 public class LocationNotFoundException extends RuntimeException {
-    public LocationNotFoundException(Object id) {
+    public LocationNotFoundException(Integer id) {
         super("Location with ID " + id + " not found.");
     }
 }
