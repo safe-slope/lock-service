03bc59079c23b2c5304c33134ebe52a0a8223a6d
Andrej R <andrej.repic04@gmail.com>
2025-12-18 17:28:00 +0100

    Added mapper methods to SkiResortController

--- lock-microservice/api/src/main/java/io/github/safeslope/api/v1/controller/SkiResortController.java
+++ lock-microservice/api/src/main/java/io/github/safeslope/api/v1/controller/SkiResortController.java
@@ -5,6 +5,7 @@ import io.github.safeslope.api.v1.dto.LockDto;
 import io.github.safeslope.api.v1.dto.LockEventDto;
 import io.github.safeslope.api.v1.dto.LockerDto;
 import io.github.safeslope.api.v1.dto.SkiResortDto;
+import io.github.safeslope.api.v1.mapper.SkiResortMapper;
 import io.github.safeslope.entities.SkiResort;
 import io.github.safeslope.skiresort.service.SkiResortService;
 import org.springframework.http.ResponseEntity;
@@ -18,35 +19,44 @@ import java.util.stream.Collectors;
 public class SkiResortController {
 
     private final SkiResortService skiResortService;
+    private final SkiResortMapper skiResortMapper;
 
-    public SkiResortController(SkiResortService skiResortService) {
+    public SkiResortController(SkiResortService skiResortService, SkiResortMapper skiResortMapper) {
         this.skiResortService = skiResortService;
+        this.skiResortMapper = skiResortMapper;
     }
 
     @GetMapping
     public List<SkiResortDto> list() {
-        return skiResortService.getAll().stream().map(this::toDto).collect(Collectors.toList());
+        return skiResortService.getAll().stream().map(skiResortMapper::toDto).collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public SkiResortDto get(@PathVariable Integer id) { return toDto(skiResortService.get(id));}
+    public SkiResortDto get(@PathVariable Integer id) {
+        return skiResortMapper.toDto(skiResortService.get(id));
+    }
 
     @GetMapping("/name/{name}")
-    public SkiResortDto getByName(@PathVariable String name) { return toDto(skiResortService.getByName(name));}
+    public SkiResortDto getByName(@PathVariable String name) {
+        return skiResortMapper.toDto(skiResortService.getByName(name));
+    }
 
     @GetMapping("/{id}/lockers")
     public List<LockerDto> getLockers(@PathVariable Integer id) {
         // TODO klici metodo v SkiResortService
+        return null;
     }
 
     @GetMapping("/{id}/locks")
     public List<LockDto> getLocks(@PathVariable Integer id) {
         // TODO klici metodo v SkiResortService
+        return null;
     }
 
     @GetMapping("/{id}/lock-events")
     public List<LockEventDto> getLockEvents(@PathVariable Integer id) {
         // TODO klici metodo v SkiResortService
+        return null;
     }
 
 
@@ -58,25 +68,8 @@ public class SkiResortController {
 
     @PostMapping
     public ResponseEntity<SkiResortDto> create(@RequestBody SkiResortDto dto) {
-        SkiResort entity = fromDto(dto);
+        SkiResort entity = skiResortMapper.toEntity(dto);
         SkiResort saved = skiResortService.create(entity);
-        return ResponseEntity.ok(toDto(saved));
+        return ResponseEntity.ok(skiResortMapper.toDto(saved));
     }
-
-
-    private SkiResortDto toDto(SkiResort s) {
-        return new SkiResortDto(
-                s.getId(),
-                s.getName(),
-                s.getAddress()
-        );
-    }
-
-    private SkiResort fromDto(SkiResortDto d){
-        return SkiResort.builder()
-                .name(d.getName())
-                .address(d.getAddress())
-                .build();
-    }
-
 }
