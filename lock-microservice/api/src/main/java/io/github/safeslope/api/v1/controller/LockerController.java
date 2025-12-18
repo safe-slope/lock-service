c851abd03424c0710643c5c77b2932c4012b923a
Andrej R <andrej.repic04@gmail.com>
2025-12-18 14:53:53 +0100

    Added the update method for LockerController

--- lock-microservice/api/src/main/java/io/github/safeslope/api/v1/controller/LockerController.java
+++ lock-microservice/api/src/main/java/io/github/safeslope/api/v1/controller/LockerController.java
@@ -3,7 +3,9 @@ package io.github.safeslope.api.v1.controller;
 import io.github.safeslope.api.v1.dto.LockDto;
 import io.github.safeslope.api.v1.dto.LockerDto;
 import io.github.safeslope.entities.Locker;
+import io.github.safeslope.entities.SkiResort;
 import io.github.safeslope.locker.service.LockerService;
+import io.github.safeslope.skiresort.service.SkiResortService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,9 +17,11 @@ import java.util.stream.Collectors;
 public class LockerController {
 
     private final LockerService lockerService;
+    private final SkiResortService skiResortService;
 
-    public LockerController(LockerService lockerService) {
+    public LockerController(LockerService lockerService, SkiResortService skiResortService) {
         this.lockerService = lockerService;
+        this.skiResortService = skiResortService;
     }
 
     @GetMapping
@@ -28,6 +32,11 @@ public class LockerController {
     @GetMapping("/{id}")
     public LockerDto get(@PathVariable Integer id) {
         return toDto(lockerService.get(id));
+    }
+
+    @PutMapping("/{id}")
+    public LockerDto update(@PathVariable Integer id, @RequestBody LockerDto dto) {
+        return toDto(lockerService.update(id, fromDto(dto)));
     }
 
     @GetMapping("/mac/{mac}")
@@ -53,5 +62,18 @@ public class LockerController {
                 l.getMacAddress(),
                 l.getSkiResort() != null ? l.getSkiResort().getId() : null
         );
+    }
+
+    private Locker fromDto(LockerDto ld) {
+        SkiResort resort = null;
+        if (ld.getSkiResortId() != null) {
+            resort = skiResortService.get(ld.getSkiResortId());
+        }
+
+        return Locker.builder()
+                .macAddress(ld.getMacAddress())
+                .dateAdded(ld.getDateAdded())
+                .skiResort(resort)
+                .build();
     }
 }
