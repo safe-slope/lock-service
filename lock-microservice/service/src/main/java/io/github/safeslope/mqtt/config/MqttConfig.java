519eb0bd5b27dc30839fde5c761b67338e9e971e
nicknick75 <nika.kralj27@gmail.com>
2025-12-20 20:34:31 +0100

    config file for MQTT

---
+++ lock-microservice/service/src/main/java/io/github/safeslope/mqtt/config/MqttConfig.java
@@ -0,0 +1,20 @@
+package io.github.safeslope.service.mqtt;
+
+import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(MqttProperties.class)
+public class MqttConfig {
+    @Bean
+    public MqttConnectOptions mqttConnectOptions(MqttProperties properties) {
+        
+    }
+
+}
