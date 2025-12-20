e75113aafb2f07d7f674dacc3e2a559e30962557
nicknick75 <nika.kralj27@gmail.com>
2025-12-20 20:41:31 +0100

    Updated file for MQTT

--- lock-microservice/service/src/main/java/io/github/safeslope/mqtt/config/MqttConfig.java
+++ lock-microservice/service/src/main/java/io/github/safeslope/mqtt/config/MqttConfig.java
@@ -1,10 +1,6 @@
 package io.github.safeslope.service.mqtt;
 
-import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
-import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.eclipse.paho.client.mqttv3.*;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +8,35 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties(MqttProperties.class)
 public class MqttConfig {
-    @Bean
-    public MqttConnectOptions mqttConnectOptions(MqttProperties properties) {
-        
-    }
 
+    @Bean
+    public IMqttAsyncClient mqttClient(MqttProperties props) throws MqttException {
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true);
+        options.setCleanSession(props.isCleanSession());
+
+        if (props.getUsername() != null && !props.getUsername().isBlank()) {
+            options.setUserName(props.getUsername());
+        }
+        if (props.getPassword() != null && !props.getPassword().isBlank()) {
+            options.setPassword(props.getPassword().toCharArray());
+        }
+
+        IMqttAsyncClient client =
+                new MqttAsyncClient(props.getBroker(), props.getClientId());
+
+        client.connect(options, null, new IMqttActionListener() {
+            @Override
+            public void onSuccess(IMqttToken asyncActionToken) {
+                System.out.println("[MQTT] Connected");
+            }
+
+            @Override
+            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                System.err.println("[MQTT] Connection failed: " + exception.getMessage());
+            }
+        });
+
+        return client;
+    }
 }
