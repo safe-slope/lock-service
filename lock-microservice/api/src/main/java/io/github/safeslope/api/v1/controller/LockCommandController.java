f44f47a97433016586c3c36e10ff05e66245e2a9
nicknick75 <nika.kralj27@gmail.com>
2025-12-20 19:42:21 +0100

    LockCommandController - base for commanding lock/unlock

---
+++ lock-microservice/api/src/main/java/io/github/safeslope/api/v1/controller/LockCommandController.java
@@ -0,0 +1,14 @@
+package io.github.safeslope.api.v1.controller;
+
+import io.github.safeslope.lock.service.LockCommandService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@RequestMapping("/api/v1/locks")
+public class LockCommandController {
+
+}
