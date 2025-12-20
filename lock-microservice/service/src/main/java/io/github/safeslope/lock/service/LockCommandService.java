4428577dd1d26fcbdccebec1c1feeb1074cbdbf5
nicknick75 <nika.kralj27@gmail.com>
2025-12-20 23:31:06 +0100

    Add LockCommandService

---
+++ lock-microservice/service/src/main/java/io/github/safeslope/lock/service/LockCommandService.java
@@ -0,0 +1,35 @@
+package io.github.safeslope.lock.service;
+
+import io.github.safeslope.entities.Lock;
+import io.github.safeslope.service.mqtt.MqttLockAdapter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LockCommandService {
+
+    private final LockService lockService;
+    private final MqttLockAdapter mqtt;
+
+    public LockCommandService(LockService lockService, MqttLockAdapter mqtt) {
+        this.lockService = lockService;
+        this.mqtt = mqtt;
+    }
+
+    public void unlock(Integer lockId) {
+        Lock lock = lockService.getLock(lockId);
+
+        String tenantId = lock.getLocker().getSkiResort().getId().toString(); 
+        String lockKey = lock.getMacAddress();
+
+        mqtt.sendCommand(tenantId, lockKey, "{\"cmd\":\"UNLOCK\"}");
+    }
+
+    public void lock(Integer lockId) {
+        Lock lock = lockService.getLock(lockId);
+
+        String tenantId = lock.getLocker().getSkiResort().getId().toString();
+        String lockKey = lock.getMacAddress();
+
+        mqtt.sendCommand(tenantId, lockKey, "{\"cmd\":\"LOCK\"}");
+    }
+}
