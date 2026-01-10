f376cb9596cc3fc1e95b2c60771cf86aef33b886
Andrej R <andrej.repic04@gmail.com>
2026-01-10 14:20:25 +0100

    Deleted unused classes

--- lock-microservice/mqtt-adapter/src/main/java/io/github/safeslope/mqtt/MqttDomainHandler.java
+++
@@ -1,7 +0,0 @@
-package io.github.safeslope.mqtt;
-
-import io.github.safeslope.mqtt.dto.MqttMessage;
-
-public interface MqttDomainHandler {
-    void onLockEvent(String tenantId, String resortId, String lockerId, MqttMessage dto);
-}
