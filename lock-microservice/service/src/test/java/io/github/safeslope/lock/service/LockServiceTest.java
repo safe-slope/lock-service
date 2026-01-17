a626341b262e9f429e1acfd27567bbb7e8d80460
nicknick75 <nika.kralj27@gmail.com>
2026-01-17 20:20:14 +0100

    test: set up junit5 test infrastructure for module service

---
+++ lock-microservice/service/src/test/java/io/github/safeslope/lock/service/LockServiceTest.java
@@ -0,0 +1,13 @@
+package io.github.safeslope.lock.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LockServiceTest {
+
+    @Test
+    void sanity() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
