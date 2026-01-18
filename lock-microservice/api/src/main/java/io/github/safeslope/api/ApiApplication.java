e4aa618c48373823d2fd18596da57b53d970a1e8
Andrej R <andrej.repic04@gmail.com>
2026-01-19 00:01:39 +0100

    chore: enable JPA repositories and entity scanning in ApiApplication for Spring Data support

--- lock-microservice/api/src/main/java/io/github/safeslope/api/ApiApplication.java
+++ lock-microservice/api/src/main/java/io/github/safeslope/api/ApiApplication.java
@@ -2,10 +2,14 @@ package io.github.safeslope.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "io.github.safeslope")
 @ConfigurationPropertiesScan(basePackages = "io.github.safeslope.mqtt.config")
+@EnableJpaRepositories(basePackages = "io.github.safeslope")
+@EntityScan(basePackages = "io.github.safeslope.entities")
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
